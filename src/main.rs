ae2dce5452c1d1cf7e12c7078f5bc086a72ddd66
Zoran Lazarevic <lazarevic.zoki91@gmail.com>
2020-08-27 22:31:10 +0200

    added clap crate, initial cli parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate lazy_static;
+extern crate clap;
 extern crate log;
 extern crate log4rs;
 extern crate tch;
@@ -10,15 +11,57 @@ mod image_ops;
 mod utils;
 
 use anyhow::Result;
+use clap::{App, Arg, SubCommand};
+
+const CHAR_REC_SC: &str = "char-rec";
+const TEXT_DETECTION_SC: &str = "text-detection";
+const IMAGE_OPS_SC: &str = "image-ops";
 
 fn main() -> Result<()> {
     log4rs::init_file("log4rs.yml", Default::default())?;
 
-    // image_ops::preprocess_image("Screen Shot 2020-08-27 at 5.37.59 PM.png")?;
+    let matches = App::new("CLI for ocr-rs")
+        .version("0.0.1")
+        .author("Zoran Lazarevic <lazarevic.zoki91@gmail.com>")
+        .about("A CLI tool for training/evaluating models for character recognition/text detection/both")
+        .subcommand(
+            SubCommand::with_name("char-rec")
+                .about("Controls for the character recognition model")
+                // .arg(
+                //     Arg::with_name("debug")
+                //         .short("d")
+                //         .help("print debug information verbosely"),
+                // ),
+        )
+        .subcommand(
+            SubCommand::with_name("text-detection")
+                .about("Controls for the text detection model")
+                // .arg(
+                //     Arg::with_name("debug")
+                //         .short("d")
+                //         .help("print debug information verbosely"),
+                // ),
+        )
+        .subcommand(
+            SubCommand::with_name("image-ops")
+                .about("Offers some useful image operations")
+                // .arg(
+                //     Arg::with_name("debug")
+                //         .short("d")
+                //         .help("print debug information verbosely"),
+                // ),
+        )
+        .get_matches();
 
-    let image_tensor =
-        image_ops::load_image_as_tensor("images/test/akronim-dist-1-regular-upper-M-img.png")?;
-    char_rec_conv_nn::run_prediction(&image_tensor)?;
+    if let Some(_matches) = matches.subcommand_matches(CHAR_REC_SC) {
+        let image_tensor =
+            image_ops::load_image_as_tensor("images/test/akronim-dist-1-regular-upper-M-img.png")?;
+        char_rec_conv_nn::run_prediction(&image_tensor)?;
+    // } else if let Some(matches) = matches.subcommand_matches(TEXT_DETECTION_SC) {
+    //
+    } else if let Some(_matches) = matches.subcommand_matches(IMAGE_OPS_SC) {
+        image_ops::preprocess_image("Screen Shot 2020-08-27 at 5.37.59 PM.png")?;
+    }
 
     Ok(())
 }
